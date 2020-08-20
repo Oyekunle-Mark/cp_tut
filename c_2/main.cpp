48d103568023a83ea35432d885f4e3edb6860847
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-08-20 09:29:55 +0100

    Added both files

--- c_2/main.cpp
+++ c_2/main.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
 
+int& readNumber();
+void writeAnswer(int& input);
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int firstNumber {readNumber()};
+    int secondNumber {readNumber()};
+    int sum = firstNumber + secondNumber;
+
+    writeAnswer(sum);
+
     return 0;
+}
+
+int& readNumber() {
+    std::cout << "Enter an integer: ";
+    int input {};
+    std::cin >> input;
+
+    return input;
+}
+
+void writeAnswer(int& input) {
+    std::cout << "The sum of the two numbers are: " << input << std::endl;
 }
