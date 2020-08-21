ca8006a2d97d9cb0a0121280ea08a22cb88bfde1
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-08-21 18:06:25 +0100

    Forward declarations

--- falling_ball/main.cpp
+++ falling_ball/main.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
 
+double getTowerHeight();
+double calculateHeight(double towerHeight, int seconds);
+void printHeight(double height);
+void calculateAndPrintHeight(double towerHeight, int seconds);
+
 int main() {
     std::cout << "Hello, World!" << std::endl;
     return 0;
