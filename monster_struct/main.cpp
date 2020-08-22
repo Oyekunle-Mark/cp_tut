8c38131a1ecd0430e1d67efabec843c1c0bb7263
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-08-22 17:54:20 +0100

    Completed program

--- monster_struct/main.cpp
+++ monster_struct/main.cpp
@@ -1,20 +1,33 @@
 #include <iostream>
 #include <string>
 
-int main() {
-    enum class MonsterType {
-        Ogre,
-        Dragon,
-        Orc,
-        Spider,
-        Slime,
-    };
+enum class MonsterType {
+    Ogre,
+    Dragon,
+    Orc,
+    Spider,
+    Slime,
+};
 
-    struct Monster {
-        MonsterType type;
-        std::string name;
-        size_t health;
-    };
+struct Monster {
+    MonsterType type;
+    std::string name;
+    size_t health;
+};
+
+void printMonster(const Monster &monster) {
+    std::cout << "This " << monster.type
+              << " is named " << monster.name
+              << " and has " << monster.health
+              << " health.\n";
+}
+
+int main() {
+    Monster borg{MonsterType::Ogre, "Borg", 127};
+    Monster lum{MonsterType::Slime, "Lum", 79};
+
+    printMonster(borg);
+    printMonster(lum);
 
     return 0;
 }
