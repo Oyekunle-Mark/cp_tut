7d0f9c0269097f66d4af64493a3a2338cadc95a7
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-08-23 17:06:43 +0100

    Created header file

--- hi-lo/hi-lo-utils.h
+++ hi-lo/hi-lo-utils.h
@@ -5,10 +5,25 @@
 #ifndef HI_LO_HI_LO_UTILS_H
 #define HI_LO_HI_LO_UTILS_H
 
+#include <cstdlib>
+#include <random>
+
 namespace constants {
-    inline constexpr int tryCount {7};
-    inline constexpr int minGuess {1};
-    inline constexpr int maxGuess {100};
+    inline constexpr size_t tryCount {7};
+    inline constexpr size_t minGuess {1};
+    inline constexpr size_t maxGuess {100};
 }
+
+enum class Result {
+    Low,
+    Correct,
+    High,
+};
+
+size_t getGuess(size_t guessCount);
+char getChoice();
+Result compareGuess(size_t userInput);
+void runGameLoop();
+int generateRandomNumber();
 
 #endif //HI_LO_HI_LO_UTILS_H
