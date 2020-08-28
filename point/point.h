cfef1fb6adefa0e1ddd279eda18a9aa53143f02b
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-08-28 20:14:06 +0100

    Class header

--- point/point.h
+++ point/point.h
@@ -5,4 +5,20 @@
 #ifndef POINT_POINT_H
 #define POINT_POINT_H
 
+#include <ostream>
+
+class Point {
+private:
+    int m_x;
+    int m_y;
+public:
+    Point(int x, int y);
+
+    Point &operator-();
+
+    Point &operator=(const Point &rhs);
+
+    friend std::ostream &operator<<(const std::ostream &out, const Point &rhs);
+};
+
 #endif //POINT_POINT_H
