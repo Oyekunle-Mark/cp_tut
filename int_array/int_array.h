8fe1bde1a7de49f6ec4a8fae316d642493694f17
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-08-28 20:48:26 +0100

    Created class header

--- int_array/int_array.h
+++ int_array/int_array.h
@@ -5,4 +5,23 @@
 #ifndef INT_ARRAY_INT_ARRAY_H
 #define INT_ARRAY_INT_ARRAY_H
 
+#include <cstddef>
+#include <ostream>
+
+class IntArray {
+private:
+    std::size_t m_size;
+    int *array;
+public:
+    IntArray(std::size_t size);
+
+    IntArray(const IntArray &copy);
+
+    int &operator[](int index);
+
+    friend std::ostream &operator<<(std::ostream out, const IntArray &rhs);
+
+    ~IntArray();
+};
+
 #endif //INT_ARRAY_INT_ARRAY_H
