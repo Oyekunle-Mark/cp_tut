c77ca4f084a6b2ebcc7489f7a141c549f39320af
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-09-01 20:44:42 +0100

    Completed the creature header

--- c_11/creature.h
+++ c_11/creature.h
@@ -4,10 +4,25 @@
 
 #ifndef C_11_CREATURE_H
 #define C_11_CREATURE_H
+#include <string>
 
-
-class creature {
-
+class Creature {
+private:
+    std::string name;
+    char symbol;
+    int health;
+    int damage;
+    int gold;
+public:
+    Creature(const std::string& name, char symbol, int health, int damage, int gold);
+    const std::string& getName() const;
+    char getSymbol() const;
+    int getHealth() const;
+    int getDamage() const;
+    int getGold() const;
+    void reduceHealth(int amount);
+    bool isDead() const;
+    void addGold(int amount);
 };
 
 
