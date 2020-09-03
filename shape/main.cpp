5f545d875f0bec148afd8269c4947613ca85c38e
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-09-03 17:33:39 +0100

    Added the circle class

--- shape/main.cpp
+++ shape/main.cpp
@@ -1,7 +1,18 @@
 #include "shape.h"
+#include "point.h"
 
 class Circle : public Shape {
+private:
+    Point m_center;
+    int m_radius;
+public:
+    Circle(const Point &center, int radius)
+            : m_center{center}, m_radius{radius} {}
 
+    std::ostream &print(std::ostream &out) const override {
+        out << "(" << m_center << "radius " << m_radius << ")\n";
+        return out;
+    }
 };
 
 int main() {
