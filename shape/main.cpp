8913a8dc874594ff4bad1e536a16cc51aa317496
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-09-03 17:36:47 +0100

    Added the triangle class

--- shape/main.cpp
+++ shape/main.cpp
@@ -15,6 +15,21 @@ public:
     }
 };
 
+class Triangle : public Shape {
+private:
+    Point m_p1;
+    Point m_p2;
+    Point m_p3;
+public:
+    Triangle(const Point &p1, const Point &p2, const Point &p3)
+            : m_p1{p1}, m_p2{p2}, m_p3{p3} {}
+
+    std::ostream &print(std::ostream &out) const override {
+        out << "(" << m_p1 << m_p2 << m_p3 << ")";
+        return out;
+    }
+};
+
 int main() {
     std::cout << "Hello, World!" << std::endl;
     return 0;
