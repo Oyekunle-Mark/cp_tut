127637ffcc7590e26bf4d5e3acdccbe755740151
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-09-04 18:20:47 +0100

    Created pair1 class

--- pairs_all_over/main.cpp
+++ pairs_all_over/main.cpp
@@ -1,5 +1,22 @@
 #include <iostream>
 
+template<class T>
+class Pair1 {
+    T m_first;
+    T m_second;
+public:
+    Pair1(T first, T second)
+            : m_first{first}, m_second{second} {}
+
+    T first() const {
+        return m_first;
+    }
+
+    T second() const {
+        return m_second;
+    }
+};
+
 int main() {
     std::cout << "Hello, World!" << std::endl;
     return 0;
