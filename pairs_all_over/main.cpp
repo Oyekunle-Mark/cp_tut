9db5038457d1b63449715401ea2daf7b64c6036e
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-09-04 18:42:56 +0100

    instantiated StringValuePair class

--- pairs_all_over/main.cpp
+++ pairs_all_over/main.cpp
@@ -26,11 +26,8 @@ public:
 };
 
 int main() {
-    const Pair<int, double> p1{5, 6.7};
-    std::cout << "Pair: " << p1.first() << ' ' << p1.second() << '\n';
-
-    const Pair<double, int> p2{2.3, 4};
-    std::cout << "Pair: " << p2.first() << ' ' << p2.second() << '\n';
+    StringValuePair<int> svp("Hello", 5);
+    std::cout << "Pair: " << svp.first() << ' ' << svp.second() << '\n';
 
     return 0;
 }
