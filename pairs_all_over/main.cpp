f7868c0dc2b31d9440bb3572e1d841529e61a6fb
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-09-04 18:22:42 +0100

    instantiated pair1 class

--- pairs_all_over/main.cpp
+++ pairs_all_over/main.cpp
@@ -18,6 +18,11 @@ public:
 };
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    const Pair1<int> p1{5, 8};
+    std::cout << "Pair: " << p1.first() << ' ' << p1.second() << '\n';
+
+    const Pair1<double> p2{2.3, 4.5};
+    std::cout << "Pair: " << p2.first() << ' ' << p2.second() << '\n';
+
     return 0;
 }
